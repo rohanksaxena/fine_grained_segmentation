b0178eb393cfc5e2440ed5fa359ff588f378dafa
rohanksaxena <rohansaxena3107@gmail.com>
2023-02-27 21:24:12 +0100

    Add training script for SSN+MLP

--- test_vgg.py
+++ test_vgg.py
@@ -9,7 +9,7 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 from torchvision import transforms as T
-from torchvision.models import vgg16, VGG16_Weights
+from torchvision.models import vgg16
 from torchvision.models.vgg import *
 from torchvision.models.vgg import model_urls, cfgs
 from torch.utils.data import DataLoader
@@ -23,7 +23,8 @@ import argparse
 from lib.dataset import bsds, augmentation
 from lib.utils.meter import Meter
 from lib.utils.loss import reconstruct_loss_with_cross_etnropy, reconstruct_loss_with_mse
-from model import SSNModel, SSN_VGG
+from model import SSNModel, SSN_VGG, MLP
+from graph import Graph, Superpixel
 
 relu_layers = (1, 3, 6, 8, 11, 13, 15, 18, 20, 22, 25, 27, 29)
 pooling_layers = (4, 9, 16, 23, 30)
@@ -66,12 +67,12 @@ def eval(model, loader, color_scale, pos_scale, device):
 
         inputs = torch.cat([color_scale * inputs, pos_scale * coords], 1)
 
-        Q, H, superpixel_features, extracted_features = model(inputs)
+        Q, H, superpixel_features, num_spixels_width = model(inputs)
 
-        print('Q: ', Q.shape)
-        print('H: ', H.shape)
-        print('superpixel features: ', superpixel_features.shape)
-        print('extracted features: ', extracted_features.shape)
+        # print('Q: ', Q.shape)
+        # print('H: ', H.shape)
+        # print('superpixel features: ', superpixel_features.shape)
+        # print('extracted features: ', extracted_features.shape)
 
         H = H.reshape(height, width)
         labels = labels.argmax(1).reshape(height, width)
@@ -92,9 +93,11 @@ def custom_forward(model, x):
     return output
 
 
-def update_params(data, model, optimizer, compactness, color_scale, pos_scale, device):
+def update_params(data, model, optimizer, compactness, color_scale, pos_scale, device, mlp_model, batch_size):
     inputs, labels = data
-
+    # print('In update')
+    # print(f'input shape: {inputs.shape}')
+    # print(f'labels shape: {labels.shape}')
     inputs = inputs.to(device)
     labels = labels.to(device)
 
@@ -107,25 +110,144 @@ def update_params(data, model, optimizer, compactness, color_scale, pos_scale, d
 
     inputs = torch.cat([color_scale * inputs, pos_scale * coords], 1)
 
-    Q, H, superpixel_features, extracted_features = model(inputs)
+    Q, H, superpixel_features, num_spixels_width = model(inputs)
 
+    # print(f'Q: {Q.shape}')
+    # print(f'H: {H.shape}, spixel labels: {torch.unique(H)}')
+    # print(f'superpixel_features: {superpixel_features.shape}')
+    # print(f'extracted features: {extracted_features.shape}')
+    # print(f'Q for a single pixel: {Q[0,:, 0]}')
+
+    # Extract superpixel data
+    superpixels_list = []
+    reshaped_labels = H.reshape(-1, 200, 200)
+    # print(f'Labels shape: {H.shape}, reshaped labels shape: {reshaped_labels.shape}, num_spixels_width: {num_spixels_width}')
+    edge_indices = []
+    for i in range(batch_size):
+        # print(f'Sample {i} in batch:')
+        mylist = []
+        superpixels = {}
+        for j in range(len(torch.unique(reshaped_labels))):
+            pixel_indices_2d = torch.argwhere(reshaped_labels[i, :, :] == j)
+            pixel_indices_1d = torch.argwhere(reshaped_labels[i, :, :].flatten() == j)
+            superpixels[j] = Superpixel(index=j, features=torch.unsqueeze(superpixel_features[i, :, j], 0), pixel_indices_2d=pixel_indices_2d.double(),
+                                        num_spixels_width=torch.tensor(num_spixels_width),
+                                        image_width=width, num_spixels=torch.max(reshaped_labels),
+                                        pixel_indices_1d=pixel_indices_1d.double())
+        mylist.append(superpixels)
+        edges = None
+        for key in superpixels.keys():
+            superpixel = superpixels[key]
+            # print(f'Spixel Neighbors of {superpixel.index}: ', superpixel.neighbor_spixels)
+            cds = superpixel.convert_spixel_index_to_coordinates()
+            # print(f'coords received: {coords.shape},  {coords}')
+            # edge_indices.append(coords)
+            # edge_indices += coords
+            # print(f'Current Edges: {coords}')
+            # edge_indices += coords
+            if edges is None:
+                edges = cds
+            else:
+                edges = np.hstack([edges, cds])
+
+        # edge_indices = np.asarray(edge_indices)
+        # print(f'Edge indices length: {edge_indices.shape}, type: {type(edge_indices)}, out: {edge_indices}')
+        # edges = edges.T
+        # print(f'Edge indices length: {edges.shape}, type: {type(edges)}, out: {edges}')
+
+        if(len(edge_indices) == 0):
+            edge_indices = edges
+        else:
+            np.append(edge_indices, edges)
+
+    superpixel_features = torch.transpose(torch.squeeze(superpixel_features), 0, 1)
+    # print(f'reshped superpixel features: {superpixel_features.shape}')
+    
+    edge_indices =torch.tensor(edge_indices, dtype=torch.int64)
+    edge_indices = edge_indices.to(device)
+
+    # print(f'edge indices shape: {edge_indices.shape}')
+    # print(f'num of images: {len(superpixels_list)}')
+
+    # Compute original losses
+    # Compute original recons loss
+    # print(f"Computing original recons loss: ")
+    # print(f'passing q: {Q.shape} and labels: {labels.shape}, labels vals: {torch.unique(labels)}')
     recons_loss = reconstruct_loss_with_cross_etnropy(Q, labels)
+    # print(f"original recons loss computed. ")
+    # print("Computing compactness loss:")
+    # print(f'H type: {H.type()}, H shape: {H.shape}')
     compact_loss = reconstruct_loss_with_mse(Q, coords.reshape(*coords.shape[:2], -1), H)
+    # print("compactness loss done")
 
-    loss = recons_loss + compactness * compact_loss
+
+    # Setup MLP
+    updated_spixel_features, prob_vector = mlp_model(x=superpixel_features, edge_index=edge_indices)
+    # print(f'Prob vector: {prob_vector.shape}, {prob_vector[0:10]}')
+    # print(f'labels: ', torch.unique(labels))
+    edge_indices = torch.transpose(edge_indices, 0, 1)
+    # print(f'edge indices shape: {edge_indices.shape}')
+    components = {}
+    val = 0
+    for i in range(len(edge_indices)):
+        # print(f'Checking {edge_indices[i][0]} and {edge_indices[i][1]}, p: {prob_vector[i]}')
+        if (prob_vector[i] > 0.5):
+            # print(f'Combining: {edge_indices[i][0]} and {edge_indices[i][1]}')
+            if (int(edge_indices[i][0]) in components.keys()):
+                components[int(edge_indices[i][1])] = components[int(edge_indices[i][0])]
+            elif (int(edge_indices[i][1]) in components.keys()):
+                components[int(edge_indices[i][0])] = components[int(edge_indices[i][1])]
+            else:
+                components[int(edge_indices[i][0])] = val
+                components[int(edge_indices[i][1])] = val
+                val += 1
+        else:
+            if (int(edge_indices[i][0]) not in components.keys()):
+                components[int(edge_indices[i][0])] = val
+                val += 1
+            if (int(edge_indices[i][1]) not in components.keys()):
+                components[int(edge_indices[i][1])] = val
+                val += 1
+
+    # print(f'Components: {components}')
+    
+    H_prime = H.detach().clone()
+    for i in range(len(torch.unique(H))):
+        if i in components.keys():
+            H_prime[H_prime == i] = components[i]
+
+
+    # print(f'H shape: ', H.shape)
+    # print(f'H prime shape: ', H_prime.shape)
+    # H_prime = torch.unsqueeze(H_prime, 0)
+    # print(f'H prime shape: {H_prime.shape}, values: {torch.unique(H_prime)}')
+    # print(f'labels type: {labels.type()}')
+    # print(f'H_prime type: {H_prime.type()}')
+    # H_prime = H_prime.to(torch.long)
+    # print(f'H_prime type: {H_prime.type()}')
+    # print(f"Computing comined recons loss: ")
+    # print(f'passing q: {Q.shape} and labels: {H_prime.shape}')
+    # recons_loss_with_comb_prop = reconstruct_loss_with_cross_etnropy(Q, H_prime)
+
+    recons_loss_with_comb_prop = reconstruct_loss_with_mse(Q, coords.reshape(*coords.shape[:2], -1), H_prime)
+    # print(f"Combined recons loss computed ")
+    # print(f'Combination loss: {recons_loss_with_comb_prop}')
+
+
+    loss = recons_loss + compactness * compact_loss + compactness * recons_loss_with_comb_prop
 
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
 
-    return {"loss": loss.item(), "reconstruction": recons_loss.item(), "compact": compact_loss.item()}
+    return {"loss": loss.item(), "reconstruction": recons_loss.item(), "compact": compact_loss.item(), "H_prime_recons": recons_loss_with_comb_prop.item()}
 
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--root", type=str, help="Path to dataset")
-    parser.add_argument("--batchsize", default=8, type=int, help="Batch size")
+    parser.add_argument("--batchsize", default=1, type=int, help="Batch size")
     parser.add_argument("--train_iter", default=50000, type=int)
     parser.add_argument("--lr", default=1e-4, type=float, help="Learning rate for optimizer")
     parser.add_argument("--compactness", default=1e-5, type=float)
@@ -158,12 +280,14 @@ if __name__ == '__main__':
     print(f'Number of testing samples: {len(test_loader)}')
 
     # Initialize Model
-    model = SSNModel(args.f_dim, args.n_spix, args.n_iter).to(device)
+    model = SSNModel(feature_dim=args.f_dim, nspix=args.n_spix, training=True, n_iter=args.n_iter).to(device)
+    mlp_model = MLP().to(device)
     # model = SSN_VGG(args.layer_number, args.n_spix, args.n_iter).to(device)
     print(f'Model: {model}')
 
     # Set Optimizer
-    optimizer = optim.Adam(model.parameters(), args.lr)
+    optimizer = optim.Adam(list(model.parameters()) + list(mlp_model.parameters()), args.lr)
+    # optimizer = optim.Adam(model.parameters(), args.lr)
 
     meter = Meter()
     iterations = 0
@@ -173,7 +297,7 @@ if __name__ == '__main__':
     while iterations < args.train_iter:
         for data in train_loader:
             iterations += 1
-            metric = update_params(data, model, optimizer, args.compactness, args.color_scale, args.pos_scale, device)
+            metric = update_params(data, model, optimizer, args.compactness, args.color_scale, args.pos_scale, device, mlp_model, batch_size=args.batchsize)
             meter.add(metric)
             state = meter.state(f'[{iterations}/{args.train_iter}]')
             print(state)
@@ -184,10 +308,12 @@ if __name__ == '__main__':
                     max_val_asa = asa
                     torch.save(model.state_dict(),
                                os.path.join(args.out_dir, 'best_model_.pth'))
+                    torch.save(mlp_model.state_dict(), os.path.join(args.out_dir, 'best_mlp_50.pth'))
             if iterations == args.train_iter:
                 break
         unique_id = str(int(time.time()))
         torch.save(model.state_dict(), os.path.join(args.out_dir, 'model_' + unique_id + '.pth'))
+        torch.save(mlp_model.state_dict(), os.path.join(args.out_dir, 'mlp_' + unique_id + '.pth'))
 
     # model = vgg16(weights=VGG16_Weights.IMAGENET1K_V1)
     # model.to(device)
